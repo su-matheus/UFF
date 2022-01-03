b069b29a699409d336db5de8a42a631d900b79c6
matheusreirasi <matheus.reirasi@gmail.com>
2022-01-03 13:41:29 -0300

    Não entregue

---
+++ matriz/diagonais.py
@@ -0,0 +1,5 @@
+#Faça um programa que leia uma matriz NxN de inteiros, troque os elementos da diagonal principal e da diagonal secundária, e inverta os elementos da primeira linha. Depois imprima a matriz.
+#ENTRADA: inteiro N>=2 (pode acreditar) seguidos por N*N inteiros que irão compor a matriz (primeiros N elementos são a primeira linha, os próximos N elementos são a segunda, ...)
+#SAIDA: matriz de inteiros transformada (formato padrão do python)
+
+N=int(input())
