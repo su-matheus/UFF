13f1bce62e38c289ffe01115599318ce8d9b148c
matheusreirasi <matheus.reirasi@gmail.com>
2022-01-03 11:07:09 -0300

    Laboratório de matrizes

---
+++ matriz/maior_diferença.py
@@ -0,0 +1,22 @@
+#Faça um programa que leia uma matriz NxM de inteiros, descubra o maior elemento da matriz,diminua cada elemento da matriz por este maior elemento, imprima a matriz.
+#ENTRADA: inteiros N>=2 e M>=2 (pode acreditar) seguidos por N*M inteiros que irão compor a matriz (primeiros M elementos são a primeira linha, os próximos M elementos são a segunda, ...)
+#SAIDA: matriz de inteiros transformada (formato padrão do python)
+
+nLinhas=int(input())
+nColunas=int(input())
+matriz=[]
+
+for i in range(nLinhas):
+    linha=[]
+    for j in range(nColunas):
+        linha.append(int(input()))
+    matriz.append(linha)
+
+maiorElemento=[0][0]
+
+for i in range(nLinhas):
+    for j in range(nColunas):
+        if matriz[i][j] > maiorElemento:
+            maiorElemento = matriz[i][j]
+
+print(maiorElemento)
