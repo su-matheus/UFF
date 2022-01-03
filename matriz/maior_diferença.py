89bdef60355d15dcde8f5c91b5eb7b78aaaee300
matheusreirasi <matheus.reirasi@gmail.com>
2022-01-03 13:28:53 -0300

    Exercício entregue com 66,6 porcento de acerto

--- matriz/maior_diferença.py
+++ matriz/maior_diferença.py
@@ -2,21 +2,27 @@
 #ENTRADA: inteiros N>=2 e M>=2 (pode acreditar) seguidos por N*M inteiros que irão compor a matriz (primeiros M elementos são a primeira linha, os próximos M elementos são a segunda, ...)
 #SAIDA: matriz de inteiros transformada (formato padrão do python)
 
-nLinhas=int(input())
-nColunas=int(input())
+N=int(input())
+M=int(input())
 matriz=[]
 
-for i in range(nLinhas):
+for i in range(N):
     linha=[]
-    for j in range(nColunas):
+    for j in range(M):
         linha.append(int(input()))
     matriz.append(linha)
 
 maiorElemento=[0][0]
 
-for i in range(nLinhas):
-    for j in range(nColunas):
+for i in range(N):
+    for j in range(M):
         if matriz[i][j] > maiorElemento:
             maiorElemento = matriz[i][j]
 
-print(maiorElemento)
+for i in range(N):
+    for j in range(M):
+        matriz[i][j] = matriz[i][j] - maiorElemento
+
+print(matriz)
+
+#ENTREGUE COM 66.6 PORCENTO DE ACERTO
