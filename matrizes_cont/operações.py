ff47f27d78de8f24864782066d5673d3fcd27214
matheusreirasi <matheus.reirasi@gmail.com>
2022-01-18 14:08:46 -0300

    Não entregue

---
+++ matrizes_cont/operações.py
@@ -0,0 +1,4 @@
+#Faça um programa que leia a matriz A de inteiros e de dimensão NxN. Calcule a Matriz B = AxA e depois calcule a matriz C = B+AT , onde AT é a matriz transposta de A. No fim, imprima a matriz C. Definição: matriz transposta é a matriz que se obtém da troca de linhas por colunas de uma dada matriz.
+#ENTRADA: inteiro N>=2 (pode acreditar) seguidos por N*N inteiros que irão compor a matriz (primeiros N elementos são a primeira linha, os próximos N elementos são a segunda, ...)
+#SAIDA: matriz de inteiros calculada C=B+AT (formato padrão do python)
+
