7ec0c01364ce91ab79d1bb4467c39f9656000c67
matheusreirasi <matheus.reirasi@gmail.com>
2022-01-22 15:12:00 -0300

    Não entregue

--- funcao/granizo.py
+++ funcao/granizo.py
@@ -1,1 +1,17 @@
-asdf
+def granizo(h):
+    n=1
+    valor = []
+    if (n-1)%2 == 0:
+        variavel = n-1
+        if variavel == 0:
+            valor += h
+            n += 1
+    
+    if (n-1)%2 != 0:
+        variavel = n-1
+        valor += 3*valor+1
+        n += 1
+
+h=int(input())
+granizo(h)
+print(h)
