2cabc2d8acf268f07e59fadeb7ea9a9360457c2b
matheusreirasi <matheus.reirasi@gmail.com>
2022-01-22 15:37:08 -0300

    Não entregue

--- funcao/granizo.py
+++ funcao/granizo.py
@@ -1,17 +1,21 @@
 def granizo(h):
-    n=1
-    valor = []
-    if (n-1)%2 == 0:
-        variavel = n-1
-        if variavel == 0:
-            valor += h
+    n = 1
+    while valor[n] != 1:
+        if (n-1)%2 == 0:
+            variavel = n-1
+            if variavel == 0:
+                numero = h
+                valor.append(numero)
+            else:
+                valor.append(valor[n-2]/2)
+                n += 1
+        
+        if (n-1)%2 != 0:
+            variavel = n-1
+            valor.append(3*valor[n-2])
             n += 1
-    
-    if (n-1)%2 != 0:
-        variavel = n-1
-        valor += 3*valor+1
-        n += 1
 
 h=int(input())
+valor = [0]
 granizo(h)
-print(h)
+print()
