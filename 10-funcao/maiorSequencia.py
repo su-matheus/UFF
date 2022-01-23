6c4621f331c849c0ac851ef843a52d749ee8e542
matheusreirasi <matheus.reirasi@gmail.com>
2022-01-23 17:03:32 -0300

    Não entregue

--- 10-funcao/maiorSequencia.py
+++ 10-funcao/maiorSequencia.py
@@ -1,13 +1,12 @@
 def decrescente(lista):
     sequencia = 0
-    for i in range (len(lista)+1):
+    for i in range (len(lista)-1):
         if lista[i] >= lista[i+1]:
-            lista[i+1] = lista[i]
             sequencia += 1
     return sequencia
 
 tamanhoLista = int(input())
-lista = [0]
+lista = []
 
 for i in range(tamanhoLista):
     lista.append(int(input()))
