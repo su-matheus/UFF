8352578d1132d95d49b94cf8dd58318f40db2550
matheusreirasi <matheus.reirasi@gmail.com>
2022-01-23 16:29:20 -0300

    Nao entregue

--- funcao/maiorSequencia.py
+++ funcao/maiorSequencia.py
@@ -1,15 +1,17 @@
-def decrescente(tamanhoLista):
+def decrescente(lista):
     sequencia = 0
-    for i in range (tamanhoLista):
+    for i in range (len(lista)+1):
         if lista[i] >= lista[i+1]:
+            lista[i+1] = lista[i]
             sequencia += 1
     return sequencia
 
 tamanhoLista = int(input())
-lista = []
-sequencia = decrescente(tamanhoLista)
+lista = [0]
 
 for i in range(tamanhoLista):
     lista.append(int(input()))
+
+sequencia = decrescente(lista)
 
 print(sequencia)
