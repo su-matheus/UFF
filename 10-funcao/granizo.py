722d478a3bd7e30bb5206ab093cf32eeaceb7fdc
matheusreirasi <matheus.reirasi@gmail.com>
2022-01-24 12:47:51 -0300

    Entregue

--- 10-funcao/granizo.py
+++ 10-funcao/granizo.py
@@ -1,3 +1,6 @@
+from re import I
+
+
 def granizo(h):
     n = 1
     indice = 0
@@ -12,14 +15,25 @@ def granizo(h):
                 valor.append(valor[n-1]/2)
                 indice += 1
                 n += 1
+
+        if valor[indice] == 1:
+            break
         
         if (valor[indice])%2 != 0:
             valor.append(3*valor[n-1]+1)
             indice += 1
             n += 1
 
+def maiorValorLista(valor):
+    maiorValor = 0
+    for i in range(len(valor)):
+        if valor[i] > maiorValor:
+            maiorValor = valor[i]
+    return maiorValor
+
 
 h=int(input())
 valor = [0]
 granizo(h)
-print(valor)
+maiorValor = maiorValorLista(valor)
+print('%.0f'%maiorValor)
