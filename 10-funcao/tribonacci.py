34ff202d5abd6a8021d22e98b89da626bdd7b742
matheusreirasi <matheus.reirasi@gmail.com>
2022-01-24 13:23:00 -0300

    Entregue

--- 10-funcao/tribonacci.py
+++ 10-funcao/tribonacci.py
@@ -1,7 +1,16 @@
-def tribonacci():
-    
+def tribonacci(tamanho):
+    for i in range(tamanho):
+        if i == 0:
+            lista[i] = 1
+        if i == 1:
+            lista[i] = 1
+        if i == 2:
+            lista[i] = 2
+        if i >=3:
+            lista[i] = lista[i-1] + lista[i-2] + lista[i-3]
+        
 
 tamanho = int(input())
-lista = []*tamanho
-tribonacci()
+lista = [0]*tamanho
+tribonacci(tamanho)
 print(lista)
