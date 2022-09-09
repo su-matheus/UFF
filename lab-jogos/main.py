327c5e1ff563d6c81a3f79285c6caeb457a0c31e
Matheus Felipe <matheus.reirasi@gmail.com>
2022-09-09 15:43:19 -0300

    ball moviment

--- lab-jogos/main.py
+++ lab-jogos/main.py
@@ -3,35 +3,53 @@ from PPlay.gameimage import *
 from PPlay.sprite import *
 
 #### setting window and background image ####
-window_game = Window(880,520)
-window_game.set_title("Pong")
-bg_image = GameImage("galaxy.jpg")
+janela_jogo = Window(880,520)
+janela_jogo.set_title("Pong")
+imagem_fundo = GameImage("galaxy.jpg")
 
 
 
 #### elements of the game ####
-right_player = Sprite("right_player.png", 1)
-left_player = Sprite("left_player.png", 1)
-ball = Sprite("ball.png", 1)
+jogador_direita = Sprite("right_player.png", 1)
+jogador_esquerda = Sprite("left_player.png", 1)
+bola = Sprite("ball.png", 1)
 
 
-#### ball position ####
-ball.x = window_game.width/2 - ball.width/2
-ball.y = window_game.height/2 - ball.height/2
+#### posição da bola ####
+bola.x = janela_jogo.width/2 - bola.width/2
+bola.y = janela_jogo.height/2 - bola.height/2
+
+velocidade_x = 0.25
+velocidade_y = 0.25
 
 #### player position ####
-right_player.x = window_game.width - right_player.width -15
-right_player.y = window_game.height/2 - right_player.height/2
+jogador_direita.x = janela_jogo.width - jogador_direita.width -15
+jogador_direita.y = janela_jogo.height/2 - jogador_direita.height/2
 
-left_player.x = 15
-left_player.y = window_game.height/2 - left_player.height/2
+jogador_esquerda.x = 15
+jogador_esquerda.y = janela_jogo.height/2 - jogador_esquerda.height/2
 
 while (True):
-    bg_image.draw()
-    ball.draw()
-    right_player.draw()
-    left_player.draw()
-    window_game.update()
+
+    if (bola.x > (janela_jogo.width-15)):
+        velocidade_x *= -1
+
+    if (bola.y > (janela_jogo.height-15)):
+        velocidade_y *= -1
+
+    if (bola.x < 0):
+        velocidade_x *= -1
+
+    if (bola.y < 0):
+        velocidade_y *= -1
+
+    imagem_fundo.draw()
+    bola.x += velocidade_x
+    bola.y += velocidade_y
+    bola.draw()
+    jogador_direita.draw()
+    jogador_esquerda.draw()
+    janela_jogo.update()
 
 
 ##sem o window_game.update() não é possível mostrar a imagem de fundo
