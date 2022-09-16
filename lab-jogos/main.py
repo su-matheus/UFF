d8c6a08c7714d572ebf5bcb85539138ddd2b83a8
Matheus Felipe <matheus.reirasi@gmail.com>
2022-09-16 20:30:45 -0300

    consertando bola deslizando no padding

--- lab-jogos/main.py
+++ lab-jogos/main.py
@@ -23,79 +23,110 @@ bola.y = janela_jogo.height/2 - bola.height/2
 
 
 
-velocidade_x = random.uniform(0.4, 0.7)#(a variação da velocidade da bola nesse eixo deve ser até  0.5)
-velocidade_y = random.uniform(0.6, 1.3) #(a variação da velocidade da bola nesse eixo deve ser até 1)
+#velocidade_bola_x = random.uniform(0.4, 0.7) #(a variação da velocidade da bola nesse eixo deve ser até  0.5)
+#velocidade_bola_y = random.uniform(0.6, 1.3) #(a variação da velocidade da bola nesse eixo deve ser até 1)
+
+velocidade_bola_x = random.uniform(0.5, 0.7)
+velocidade_bola_y = random.uniform(0.6, 1.1)
 
 
 
 #### posição do jogador ####
-jogador_direita.x = janela_jogo.width - jogador_direita.width -15
+jogador_direita.x = janela_jogo.width - jogador_direita.width -20
 jogador_direita.y = janela_jogo.height/2 - jogador_direita.height/2
 
-jogador_esquerda.x = 15
+jogador_esquerda.x = 20
 jogador_esquerda.y = janela_jogo.height/2 - jogador_esquerda.height/2
 
-velocidade_jogador_esquerda = 0.5
-velocidade_jogador_direita = 0.5
+velocidade_jogador_esquerda = 300
+velocidade_jogador_direita = 300
 
 teclado = Window.get_keyboard()
 
 
-print(velocidade_y)
-print(velocidade_x)
+#### pontuação ####
+placar_direito = 0
+placar_esquerdo = 0
+
+
+print("velocidade da bola em y: {}".format(velocidade_bola_y))
+print("velocidade da bola em x: {}".format(velocidade_bola_x))
+
 
 while (True):
 
     #### movimentação da bola ####
+    bola.x += velocidade_bola_x * janela_jogo.delta_time()
+    bola.y += velocidade_bola_y * janela_jogo.delta_time()
+
+
     if (bola.x > (janela_jogo.width-15)):
-        velocidade_x *= -1
+        velocidade_bola_x *= -1
 
     if (bola.y > (janela_jogo.height-15)):
-        velocidade_y *= -1
+        velocidade_bola_y *= -1
 
     if (bola.x < 0):
-        velocidade_x *= -1
+        velocidade_bola_x *= -1
 
     if (bola.y < 0):
-        velocidade_y *= -1
+        velocidade_bola_y *= -1
 
     
 
     #### movimentação do jogador direito ####
 
     if (teclado.key_pressed("UP") and jogador_direita.y >= 0):
-        jogador_direita.move_y(-velocidade_jogador_direita)
+        jogador_direita.y = jogador_direita.y - velocidade_jogador_direita*janela_jogo.delta_time()
 
     if (teclado.key_pressed("DOWN") and jogador_direita.y + jogador_direita.height <= janela_jogo.height):
-        jogador_direita.move_y(velocidade_jogador_direita)
-
+        jogador_direita.y = jogador_direita.y + velocidade_jogador_direita*janela_jogo.delta_time()
 
 
 
     #### movimentação do jogador esquerdo ####
 
     if (teclado.key_pressed("w") and jogador_esquerda.y >= 0):
-        jogador_esquerda.move_y(-velocidade_jogador_esquerda)
+        jogador_esquerda.y = jogador_esquerda.y - velocidade_jogador_esquerda * janela_jogo.delta_time()
 
     if (teclado.key_pressed("s") and jogador_esquerda.y + jogador_esquerda.height <= janela_jogo.height):
-        jogador_esquerda.move_y(velocidade_jogador_esquerda)
+        jogador_esquerda.y = jogador_esquerda.y + velocidade_jogador_esquerda * janela_jogo.delta_time()
 
 
 
     #### quando a bola tocar no jogador ####
 
-    if (bola.collided(jogador_direita)):
-        velocidade_x *= -1
+    if (bola.collided(jogador_direita) and velocidade_bola_x > 0):
+        velocidade_bola_x *= -1
 
-    if (bola.collided(jogador_esquerda)):
-        velocidade_x *= -1
+    if (bola.collided(jogador_esquerda) and velocidade_bola_x < 0):
+        velocidade_bola_x *= -1
+
+
+    #### quando alguém pontuar ####
+
+    if (bola.x <= 0):
+        bola.x = janela_jogo.width/2 - bola.width/2
+        bola.y = janela_jogo.height/2 - bola.height/2
+        placar_direito += 1
+
+        print("Pontuação do jogador direito: {}".format(placar_direito))
+        print("Pontuação do jogador esquerdo: {}".format(placar_esquerdo))
+
+    if ((bola.x + bola.width) > janela_jogo.width):
+        bola.x = janela_jogo.width/2 - bola.width/2
+        bola.y = janela_jogo.height/2 - bola.height/2
+        placar_esquerdo += 1
+        
+        print("Pontuação do jogador direito: {}".format(placar_direito))
+        print("Pontuação do jogador esquerdo: {}".format(placar_esquerdo))
 
 
 
     #### renderização do jogo ####
     imagem_fundo.draw()
-    bola.x += velocidade_x
-    bola.y += velocidade_y
+    bola.x += velocidade_bola_x
+    bola.y += velocidade_bola_y
     bola.draw()
     jogador_direita.draw()
     jogador_esquerda.draw()
