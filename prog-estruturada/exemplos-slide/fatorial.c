71de792109b96f67a2ff84edb390ab1da43b99e4
Matheus Felipe <matheus.reirasi@gmail.com>
2022-09-25 15:42:14 -0300

    nova pasta

---
+++ prog-estruturada/exemplos-slide/fatorial.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int fat (int numero) {
+    int fatorial = 1;
+
+    for (int i = 1; i<=numero; i++){
+        fatorial *= i;
+    }
+
+    return fatorial;
+}
+
+int main(void) {
+    int numero, resultado;
+    printf("Digite um número não negativo: \n");
+    scanf("%d", &numero);
+    resultado = fat(numero);
+    printf("Fatorial de %d é %d. \n", numero, resultado);
+
+    return 0; //Se tudo estiver correto retorna 0, se algo estiver errado retorna 1
+}
