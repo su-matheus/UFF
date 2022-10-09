a4dca7a02be846761f7bc7185a2fc7ce593636f4
Matheus Felipe <matheus.reirasi@gmail.com>
2022-10-08 22:09:51 -0300

    main configuration

---
+++ lab-jogos/space-invaders/globals.py
@@ -0,0 +1,17 @@
+GAME_WIDTH = 615
+GAME_HEIGHT = 412
+GAME_TITLE = "Space Wars"
+GAME_BACKGROUND_COLOR = (0,0,0)
+SPEED = 300
+
+# 1: Menu
+# 2: Play
+# 3: Difficulty
+# 4: Ranking
+# 5: Exit
+GAME_SCREEN = 1
+
+# 0: FÁCIL
+# 1: MÉDIO
+# 2: DIFÍCIL
+GAME_DIFFICULTY = 1
