829972fbf58013bb11ecd1e1d687a79f3edba372
Matheus Felipe <92674610+matheusreirasi@users.noreply.github.com>
2023-11-13 22:10:02 -0300

    Update main.c

--- prog-estruturada/Code-Blocks-Conjunto/Conjunto/main.c
+++ prog-estruturada/Code-Blocks-Conjunto/Conjunto/main.c
@@ -1,49 +1,106 @@
-#include "conjunto.h"
+#include "sets.h"
+// TAD de conjuntos
+
+
+// observe que a função aqui, no mundo da aplicacao
+// sabe quem é Tipo e pode fazer a comparacao desejada
+// Ex: comparacao de nomes de alunos, de números, de precos de mercadorias etc
+Result compareTo(Tipo a, Tipo b){
+    if (a > b) return GT;
+    if (a == b) return EQUAL;
+    return LT;
+}
+
+void imprime(Tipo t){
+    printf("%i", t); // essa função sabe quem é Tipo, no caso, um inteiro
+}
+
 
 char conjuntoAtual = 'A';
 char conjuntosExibidos[]= {'A', 'B', 'C', 'D'};
-
-void menu(){
-    system("clear");
-
-    //defineOrdemExibicaoConjuntos();
-    printf("Menu (m)\n");
-    printf("Digite diretamente os números para serem inseridos no conjunto atual.\n");
-    printf("Operação: \n");
-    printf("f: fim\n");
-    printf("u: %c union %c -> %c\n", conjuntosExibidos[0], conjuntosExibidos[1], conjuntosExibidos[2]);
-    printf("Defina o primeiro (current) set: A..Z \n");
-    printf("cojunto atual: %c\n", conjuntoAtual);
-    printf("\nCojuntos:\n");
+void defineOrdemExibicaoConjuntos(){
     int tamanho = sizeof(conjuntosExibidos)/sizeof(char);
-    /*
+    int ix = -1;
+    for (int i=0;i<tamanho;i++){
+        if (conjuntoAtual == conjuntosExibidos[i]){
+            ix = i;
+            break;
+        }
+    }
+    if (ix==-1){
+        for (int i=tamanho-1;i>0;i--){
+            // cai de prioridade na lista
+            conjuntosExibidos[i]=conjuntosExibidos[i-1];
+        }
+    } else {
+        for (int i=ix;i>=0;i--){
+            // cai de prioridade na lista
+            conjuntosExibidos[i]=conjuntosExibidos[i-1];
+        }
+    }
+    conjuntosExibidos[0]=conjuntoAtual;
+}
+void menu(){
+    system("cls");
+    printf("Menu (m)\n");
+    printf("Type int numbers followed by enter to add to current set\n");
+    printf("Operations:\ne (end)\nr (remove)\n");
+    printf("u (%c union %c into %c)\n", conjuntosExibidos[0],conjuntosExibidos[1],conjuntosExibidos[2]);
+    printf("i (%c intersection %c into %c)\nd (destroy all)\nc (clean current set)\n", conjuntosExibidos[0],conjuntosExibidos[1],conjuntosExibidos[2]);
+    printf("define first (current) set: A..Z \n");
+    printf("current set: %c\n", conjuntoAtual);
+    defineOrdemExibicaoConjuntos();
+    printf("\nSets:\n");
+    int tamanho = sizeof(conjuntosExibidos)/sizeof(char);
     for (int i=0;i<tamanho;i++){
         setsPrint(conjuntosExibidos[i], imprime); // imprimir o conjunto
     }
-    */
     printf("\n\n");
 }
 
-void inputUsuario(){
+void conjuntos(){
     char ch;
-    char strDoNumero[100];
-    int algarismo;//não tenho certezo do que seja isso
+    char strNumero[100];
+    int algarismo=0;
 
     setsCreate();
-
-    while(TRUE){
-        char cA=conjuntosExibidos[0], cB=conjuntosExibidos[1], cC=conjuntosExibidos[2];
-    }
-
-}
-
-int main()
-{
-
     menu();
-    inputUsuario();
-    return 0;
+    while (TRUE){
+        ch = getche();
+        if (ch == 'e'){
+            break;
+        } else if (((ch >= '0') && (ch <= '9')) || (ch == '.')){
+            strNumero[algarismo++] = ch;
+        } else if (ch  == 'm'){
+            menu();
+        } else if (ch == 13){ // enter
+            strNumero[algarismo] = '\0';// terminador de string
+            Tipo num = atoi(strNumero);
+            //printf("\n%i\n",num);
+            if (num != 0)
+                setsAddElement(conjuntoAtual, num);
+            algarismo = 0;
+            menu();
+        } else if (ch >= 'A' && ch <= 'Z'){
+            conjuntoAtual = ch;
+        } else if (ch == 'c'){
+            setsFreeSet(conjuntoAtual);
+            menu();
+        }  else if (ch == 'd'){
+            setsDestroy();
+            menu();
+        } else if (ch == 'u'){
+            setsUnion(conjuntosExibidos[0], conjuntosExibidos[1], conjuntosExibidos[2]);
+            menu();
+        } else if (ch == 'i'){
+            setsIntersection(conjuntosExibidos[0], conjuntosExibidos[1], conjuntosExibidos[2]);
+            menu();
+        }
+    }
 }
 
+int main(){
+    conjuntos();
+}
 
 
